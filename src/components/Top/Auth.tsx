fd39d537150ae8150400951cda2a35405c697629
A.Tanaka <ma007.okk@gmail.com>
2023-04-27 01:30:57 +0900

    Google認証　修正

--- src/components/Top/Auth.tsx
+++ src/components/Top/Auth.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState, ChangeEvent, FormEvent } from "react";
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
-  signInWithPopup,
+  signInWithRedirect,
   onAuthStateChanged,
 } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
@@ -67,7 +67,7 @@ const Auth = () => {
 
   const googleLogin = (e: any) => {
     e.preventDefault();
-    signInWithPopup(auth, provider);
+    signInWithRedirect(auth, provider);
   };
 
   const onChangeNewEmail = (e: ChangeEvent<HTMLInputElement>) => {
